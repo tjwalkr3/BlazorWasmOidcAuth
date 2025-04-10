39961e1f4a024135d270c2b838563529d0f21d81
Thomas Jones1 <thomas.jones1@students.snow.edu>
2025-04-10 13:58:41 -0600

    Add project files.

---
+++ BlazorWasmOidcAuth/CustomAuthenticationMessageHandler.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components;
+namespace BlazorWasmOidcAuth;
+
+public class CustomAuthenticationMessageHandler : AuthorizationMessageHandler
+{
+    public CustomAuthenticationMessageHandler(IAccessTokenProvider provder, NavigationManager nav) : base(provder, nav)
+    {
+        ConfigureHandler(["https://engineering.snow.edu/auth/realms/SnowCollege/"]);
+    }
+}
---
+++ BlazorWasmOidcAuth/Program.cs
@@ -0,0 +1,24 @@
+using BlazorWasmOidcAuth;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<CustomAuthenticationMessageHandler>();
+
+builder.Services.AddHttpClient("testClient", opt => opt.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                .AddHttpMessageHandler<CustomAuthenticationMessageHandler>();
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("testClient"));
+
+builder.Services.AddOidcAuthentication(opt =>
+{
+    opt.ProviderOptions.Authority = "https://engineering.snow.edu/auth/realms/SnowCollege/";
+    opt.ProviderOptions.ClientId = "JonathanMauiAuth";
+    opt.ProviderOptions.ResponseType = "code";
+    opt.ProviderOptions.DefaultScopes.Add("openid");
+    opt.ProviderOptions.DefaultScopes.Add("profile");
+});
+
+await builder.Build().RunAsync();
